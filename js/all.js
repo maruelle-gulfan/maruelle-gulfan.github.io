5bdfcca17908f56a0a104dd543b6ba9250be4ebc
maruelle <maruelle.gulfan@bpoc.co.jp>
2021-11-18 00:06:55 +0800

    Changed:add smtpjs

--- js/all.js
+++ js/all.js
@@ -36,3 +36,36 @@ $(document).ready(function(){
     });
   
   });
+
+
+  function SendEmail(name, from, proj, inquiry) {
+    Email.send({
+      SecureToken : "eb324792-5e17-4a3e-a959-f24cf793b6ac",
+      To : from,
+      From : 'maruelle08@gmail.com',
+      Subject : "email from maruelle-gulfan.github.io",
+      Body : "Name: "+name+"\n"+"Email:"+from+"\n"+"Project:"+proj +"\n"+"Message:"+inquiry,
+    }).then(
+        message => {
+            if (message !== "OK") {
+                alert(message)
+            }
+            else {
+                alert("Your inquiry has been submitted. Thank you!")
+            }
+        }
+    );
+}
+
+var emailForm = document.getElementById('email1')
+emailForm.addEventListener('submit', event => {
+
+    event.preventDefault()
+    var name = event.target[0].value
+    var from = event.target[1].value
+    var proj = event.target[2].value
+    var inquiry = event.target[3].value
+
+    SendEmail(name, from, proj, inquiry )
+})
+
