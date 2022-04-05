103a4e8214df95e3e5b11f458fd135f213a79068
maruelle-gulfan <77771444+maruelle-gulfan@users.noreply.github.com>
2022-04-05 09:35:27 +0800

    Add files via upload

--- js/all.js
+++ js/all.js
@@ -1,3 +1,25 @@
+
+const cursor = document.querySelector(".cursor");
+var timeout;
+document.addEventListener("mousemove", (e)=>{
+  let x = e.clientX;
+  let y = e.clientY;
+
+  cursor.style.top = y + "px";
+  cursor.style.left = x + "px";
+  cursor.style.display = "block";
+
+  function mouseStopped(){
+    cursor.style.display = "none";
+  }
+  clearTimeout(timeout);
+  timeout = setTimeout(mouseStopped, 3000);
+});
+
+
+
+
+
 $(document).ready(function(){
 
     $('#menu').click(function(){
@@ -41,10 +63,36 @@ $(document).ready(function(){
   function SendEmail(name, from, proj, inquiry) {
     Email.send({
       SecureToken : "eb324792-5e17-4a3e-a959-f24cf793b6ac",
-      To : from,
+      To : from+"," +'maruelle24@gmail.com',
       From : 'maruelle08@gmail.com',
       Subject : "email from maruelle-gulfan.github.io",
-      Body : "Name: "+name+"\n"+"Email:"+from+"\n"+"Project:"+proj +"\n"+"Message:"+inquiry,
+      // Body : "Name: "+name+"\nEmail:"+from+"\nProject:"+proj+"\nMessage:"+inquiry,
+      Body: `<html>
+              <head>
+              </head>
+              <body>
+                <div style=width:500px;min-height:500px;background-color:#1a1a1a;display:block;justify-content:center;color:#00B8F5;border-radius:10px;>
+                <div style="width:100%;text-align:center;padding-top:1rem;">
+                  <p style="font-size:28px;color:#00B8F5; text-align:center;">
+                    Inquiry
+                  </p>
+                </div>
+                  <div style="width:100%;color:#00B8F5;padding:1rem;">
+                    <p style="font-size:24px;margin:0">Name: <span style="font-size:18px; color:#fff; font-weight:700;">${name}</span></p> 
+                  </div>
+                  <div style="width:100%;color:#00B8F5;padding:1rem;">
+                    <p style="font-size:24px;margin:0">Email: <span style="font-size:18px; color:#fff; font-weight:700;text-decoration:none;">${from}</span></p> 
+                  </div>
+                  <div style="width:100%;color:#00B8F5;padding:1rem;">
+                    <p style="font-size:24px;margin:0">Project: <span style="font-size:18px; color:#fff; font-weight:700;">${proj}</span></p> 
+                  </div>
+                  <div style="width:100%;color:#00B8F5;padding:1rem;">
+                    <p style="font-size:24px;margin:0">Message: <span style="font-size:18px; color:#fff; font-weight:700;">${inquiry}</span></p> 
+                  </div>
+                </div>
+              
+              </body>
+            </html>`
     }).then(
         message => {
             if (message !== "OK") {
